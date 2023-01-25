efb0e8e7667924efa556362b6592a664b6e8b586
Shubham Kishor Dathia <54986627+mrdathia@users.noreply.github.com>
2023-01-25 16:43:22 +0530

    messcleared for routing and popping off contexts

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -5,7 +5,6 @@ import '../utils/AllData.dart';
 import '../utils/appTools.dart';
 import '../utils/styleConstants.dart';
 import '../app_assets_constants/AppStrings.dart';
-import 'login.dart';
 
 import 'package:intl/intl.dart';
 
@@ -49,11 +48,8 @@ class _DashboardState extends State<Dashboard> {
   }
 
   Future<bool> _onBackPressed() async {
-    return await showDialog(
-            barrierDismissible: false,
-            context: context,
-            builder: (context) => const ExitDialogue()) ??
-        false;
+    Navigator.of(context).pop();
+    return false;
   }
 
   // _iconControl(bool like) {
@@ -756,6 +752,94 @@ class _DashboardState extends State<Dashboard> {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ExitDialogue extends StatelessWidget {
+  const ExitDialogue({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      backgroundColor: hexToColor("#101010"),
+      title: Text(
+        "Exit App",
+        style: kGoogleStyleTexts.copyWith(color: Colors.white, fontSize: 18.0),
+      ),
+      content: Builder(
+        builder: (context) {
+          return SizedBox(
+            height: 100,
+            width: 200,
+            child: Column(
+              children: [
+                Text(
+                  "Are you sure you want to exit the app?",
+                  style: kGoogleStyleTexts.copyWith(
+                      color: Colors.white, fontSize: 15.0),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 15, right: 15.0, top: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 90,
+                        height: 45,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                side: const BorderSide(
+                                    width: 0.5, color: Colors.black26)),
+                            onPressed: () {
+                              Navigator.of(context).pop(false);
+                            },
+                            child: Text(
+                              "Cancel",
+                              style: kGoogleStyleTexts.copyWith(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black54,
+                                fontSize: 15.0,
+                              ),
+                            )),
+                      ),
+                      SizedBox(
+                        width: 90,
+                        height: 45,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xffC93131),
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0))),
+                            onPressed: () async {
+                              Navigator.pop(context, true);
+                            },
+                            child: Text(
+                              "Exit",
+                              style: kGoogleStyleTexts.copyWith(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontSize: 15.0,
+                              ),
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:potential/screens/CANcreationform/createAccount.dart';
 import 'package:potential/screens/login.dart';
@@ -34,93 +36,192 @@ class _RegistrationPageState extends State<RegistrationPage> {
     });
   }
 
+  Future<bool> _onBackPressed() async {
+    return await showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) => const ExitDialogue()) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ScrollConfiguration(
-      behavior: NoGlowBehaviour(),
-      child: Scaffold(
-        key: _scaffoldKey,
-        backgroundColor: hexToColor("#121212"),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: ScrollConfiguration(
+        behavior: NoGlowBehaviour(),
+        child: Scaffold(
+          key: _scaffoldKey,
+          backgroundColor: hexToColor("#121212"),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 70,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10.0),
+                          child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Get Started,",
+                                style: kGoogleStyleTexts.copyWith(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 35,
+                                  color: hexToColor("#ffffff"),
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width - 10,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: hexToColor("#0065A0"),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0))),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => CreateAccountPage(
+                                  fillEezzReq: widget.fillEezzReq),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          AppStrings.signUptext,
+                          style: kGoogleStyleTexts.copyWith(
+                              color: Colors.white, fontSize: 18.0),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width - 10,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: hexToColor("#0065A0"),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0))),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  LoginPage(fillEezzReq: widget.fillEezzReq),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          AppStrings.signInText,
+                          style: kGoogleStyleTexts.copyWith(
+                              color: Colors.white, fontSize: 18.0),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ExitDialogue extends StatelessWidget {
+  const ExitDialogue({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      backgroundColor: hexToColor("#101010"),
+      title: Text(
+        "Exit App",
+        style: kGoogleStyleTexts.copyWith(color: Colors.white, fontSize: 18.0),
+      ),
+      content: Builder(
+        builder: (context) {
+          return SizedBox(
+            height: 100,
+            width: 200,
             child: Column(
               children: [
+                Text(
+                  "Are you sure you want to exit the app?",
+                  style: kGoogleStyleTexts.copyWith(
+                      color: Colors.white, fontSize: 15.0),
+                ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Column(
+                  padding:
+                      const EdgeInsets.only(left: 15, right: 15.0, top: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const SizedBox(
-                        height: 70,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0),
-                        child: Align(
-                            alignment: Alignment.centerLeft,
+                      SizedBox(
+                        width: 90,
+                        height: 45,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                side: const BorderSide(
+                                    width: 0.5, color: Colors.black26)),
+                            onPressed: () {
+                              Navigator.of(context).pop(false);
+                            },
                             child: Text(
-                              "Get Started,",
+                              "Cancel",
                               style: kGoogleStyleTexts.copyWith(
                                 fontWeight: FontWeight.w700,
-                                fontSize: 35,
-                                color: hexToColor("#ffffff"),
+                                color: Colors.black54,
+                                fontSize: 15.0,
+                              ),
+                            )),
+                      ),
+                      SizedBox(
+                        width: 90,
+                        height: 45,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xffC93131),
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0))),
+                            onPressed: () async {
+                              exit(0);
+                            },
+                            child: Text(
+                              "Exit",
+                              style: kGoogleStyleTexts.copyWith(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontSize: 15.0,
                               ),
                             )),
                       ),
                     ],
                   ),
-                ),
-                SizedBox(
-                  height: 55,
-                  width: MediaQuery.of(context).size.width - 10,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: hexToColor("#0065A0"),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0))),
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => CreateAccountPage(
-                                fillEezzReq: widget.fillEezzReq),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        AppStrings.signUptext,
-                        style: kGoogleStyleTexts.copyWith(
-                            color: Colors.white, fontSize: 18.0),
-                      )),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 55,
-                  width: MediaQuery.of(context).size.width - 10,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: hexToColor("#0065A0"),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0))),
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                LoginPage(fillEezzReq: widget.fillEezzReq),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        AppStrings.signInText,
-                        style: kGoogleStyleTexts.copyWith(
-                            color: Colors.white, fontSize: 18.0),
-                      )),
-                ),
+                )
               ],
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
